b8da9b08213e6010d00720440bc637d13b58e484
Everest1508 <riteshmahale15@gmail.com>
2024-04-21 00:57:02 +0530

    flutter-auth

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+
+urlpatterns = [
+    path('register/',RegisterUserAPIView.as_view()),
+    path('verify/',VerifyOTPAPIView.as_view()),
+    path('login/',LoginUserAPIView.as_view()),
+    # path('myprofile/',UserProfileAPIView.as_view()),
+    path('forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
+    path('reset-password/', ResetPasswordAPIView.as_view(), name='reset_password'),
+]
